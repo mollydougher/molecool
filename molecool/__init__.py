36fb7261bdf07150ae94aac0f2a8b964e4fa45d9
Molly Dougher <mdoughe6@nd.edu>
2023-06-12 14:39:32 -0400

    Split up the functions into modules

--- molecool/__init__.py
+++ molecool/__init__.py
@@ -3,6 +3,11 @@ This was created for the Python Best Practices Workshop."""
 
 # Add imports here
 from .functions import canvas
-
+from molecool.measure import calculate_distance, calculate_angle
+from molecool.atom_data import atom_colors, atomic_weights
+from molecool.visualization import draw_molecule
+from molecool.molecules import bond_histogram, build_bond_list
+from molecool.io import open_pdb
+# from molecool import io --> this will also work
 
 from ._version import __version__
